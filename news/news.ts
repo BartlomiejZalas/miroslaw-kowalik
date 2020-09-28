bb09dfa57333d477e2b3f374effdb46c61618d13
Bartlomiej Zalas <b.zalas@zoovu.com>
2020-09-28 20:59:01 +0200

    news kultura w sieci 3

--- news/news.ts
+++ news/news.ts
@@ -6,6 +6,7 @@ export interface News {
 }
 
 export const NEWS: News[] = [
+  { id: 'kultura-w-sieci3', date: '28-09-2020', title: 'Kultura w Sieci', componentName: 'NewsKulturaWSieci3' },
   { id: 'kultura-w-sieci2', date: '31-08-2020', title: 'Kultura w Sieci', componentName: 'NewsKulturaWSieci2' },
   { id: 'kultura-w-sieci1', date: '30-07-2020', title: 'Kultura w Sieci', componentName: 'NewsKulturaWSieci1' },
   { id: 'nowa-kultura', date: '10-07-2020', title: 'Nowa Klutura', componentName: 'NewsNowaKultura' },
